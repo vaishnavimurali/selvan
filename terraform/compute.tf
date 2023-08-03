d605b44e973bca58bd60574ab4f0405281863426
subbu <vaishnavimurali457@gmail.com>
2023-08-03 19:49:32 +0530

    updating name

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -3,7 +3,7 @@ data "aws_ami" "amazon-linux-2" {
   most_recent = true
   owners = ["amazon"]
   filter {
-    name = "name"
+    name = "subbu"
     values = [
       "amzn2-ami-hvm-*-x86_64-gp2",
     ]
